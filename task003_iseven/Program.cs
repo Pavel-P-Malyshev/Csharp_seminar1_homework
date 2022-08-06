020726eb00ac6ce1d78ff903fb84fe03219b8c0b
Pavel <m83819@gmail.com>
2022-08-06 09:42:33 +0700

    Task003_iseven

---
+++ task003_iseven/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> Четное
+
+*/
+
+Console.WriteLine("Enter  your number");
+int A = int.Parse(Console.ReadLine()); //преобразование строки в целове число
+
+
+if(A%2==0) {Console.WriteLine("Your number is even");}
+else {Console.WriteLine("Your number is odd");}
